7ced28b2d8117472ea07e2d5d49ec6d1ac180b75
Peter <github@loginp.me>
2024-11-06 07:55:45 -0600

    Update shop.py

--- shop.py
+++ shop.py
@@ -18,6 +18,15 @@ class ShopItem:
         self.cpc = cpc # Cookies per click (multiple)
         self.purchased_count = 0  # Track how many times this item has been purchased
 
+# Class for shop upgrades, which are one-time purchases
+class ShopUpgrade:
+    def __init__(self, name, base_cost, cps, cpc):
+        self.name = name # Item Name
+        self.cost = base_cost # Cost to purchase
+        self.cps = cps # Cookies per second
+        self.cpc = cpc # Cookies per click (multiple)
+        self.purchased_count = 0  # Track how many times this item has been purchased
+
 # price = base x 1.15 ^ (buildings owned)
 shop_items = {
     'Extra Hands': ShopItem("Extra Hands", 100, None, 2),
