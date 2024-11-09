74a1375366b20da6984d3442076b7b9e30ed8315
i175w693 <123773539+i175w693@users.noreply.github.com>
2024-11-08 21:18:52 -0600

    Added shop upgrade and cookies per click saving

--- save_game.py
+++ save_game.py
@@ -4,7 +4,7 @@ Purpose: This module is used to save the user's profile to a save file
 Inputs: None
 Output: None
 Additional code sources: 
-Developers: Peter Pham
+Developers: Peter Pham, Ian Wilson
 Date: 10/26/2024
 Last Modified: 10/26/2024
 '''
@@ -15,7 +15,9 @@ import time
 def save(ui_manager):
     with open('save.txt', 'w') as file:
         # saves the user's current balance along with any shop purchases they have made
-        output = f'{time.time()}\n{ui_manager.cookie_count}\n'
+        output = f'{time.time()}\n{ui_manager.cookie_count}\n{ui_manager.base_cookie_per_click}\n{ui_manager.click_multiplier}\n'
         for item in ui_manager.shop_items.values():
             output += f'"{item.name}":{item.purchased_count}\n'
+        for upgrade in ui_manager.shop_upgrades.values():
+            output += f'"{upgrade.name}":{upgrade.purchased_count}\n'
         file.write(output)
